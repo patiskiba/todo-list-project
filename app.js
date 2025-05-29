eb132dceccffc3b6ef5bb349302e7ebe41f49b11
patiskiba <patricia.skiba@mscoding.org>
2025-05-29 12:17:25 -0500

    Added innerHTML to li
    This makes the list item actually show up

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-let createCategoryButton = document.getElementById(createCategoryButton);
+
+//! Create tasks
+
+//? Define create task buttons
+let createTaskButtons = document.querySelector(".createTaskInCategory-btn");
+console.log(createTaskButtons);
+//? Define list containers
+let categoryLists = document.querySelector(".categoryList ul");
+console.log(categoryLists);
 
 
 
+//? Event listener for each create task button
 
-// Inside each category card
-let createTaskButton = document.querySelectorAll("createTaskInCategory-btn");
+    createTaskButtons.addEventListener("click", () => {
+        console.log("Clicked");
+        let newListItem = document.createElement("li");
+        
+        categoryLists.appendChild(newListItem);
+        
+        let newListItemInner = `<input type="checkbox"> <input type="text" placeholder="Task list item" class="listText">`;
 
+        newListItem.innerHTML = newListItemInner;
+    })
